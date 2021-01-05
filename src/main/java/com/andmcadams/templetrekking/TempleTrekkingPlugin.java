4ae76efa48948241fcee12acc207450bc40e9dda
Andrew McAdams <andmcadams@gmail.com>
2021-01-05 00:09:17 -0600

    Remove unused imports and fix style

--- src/main/java/com/andmcadams/templetrekking/TempleTrekkingPlugin.java
+++ src/main/java/com/andmcadams/templetrekking/TempleTrekkingPlugin.java
@@ -1,16 +1,10 @@
 package com.andmcadams.templetrekking;
 
-import com.google.inject.Provides;
 import javax.inject.Inject;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
-import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
-import net.runelite.api.GameState;
-import net.runelite.api.VarbitComposition;
-import net.runelite.api.events.GameStateChanged;
 import net.runelite.api.events.VarbitChanged;
-import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
@@ -38,6 +32,7 @@ public class TempleTrekkingPlugin extends Plugin
 	private int lastPoints;
 
 	private static final int TEMPLE_TREKKING_POINTS = 1955;
+
 	@Override
 	protected void startUp() throws Exception
 	{
@@ -61,7 +56,9 @@ public class TempleTrekkingPlugin extends Plugin
 
 		// If the current point count is nonzero, but the new count is zero, the trek ended.
 		if (tempPoints == 0 && currentPoints != 0)
+		{
 			lastPoints = currentPoints;
+		}
 		currentPoints = tempPoints;
 	}
 }
